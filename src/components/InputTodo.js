31746261a1e057ed377d4a5e83885c22e126405f
AbeeraTahir <abeeratahir2018@gmail.com>
2022-12-05 22:55:52 +0500

    Add to-do input form

---
+++ src/components/InputTodo.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+/* eslint-disable */
+class InputTodo extends Component {
+  state = {
+    title: ''
+  };
+  onChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+  render() {
+    return (
+      <form>
+        <input type="text" placeholder="Add Todo..." value={this.state.title} name="title" onChange={this.onChange}/>
+        <button>Submit</button>
+      </form>
+    )
+  }
+}
+export default InputTodo
