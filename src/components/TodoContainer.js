4dec7f9830f6a5f5979f43751769eee4f3446836
AbeeraTahir <abeeratahir2018@gmail.com>
2022-12-05 23:37:46 +0500

    Add styles and fix toggle error

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -29,17 +29,19 @@ class TodoContainer extends React.Component {
     };
   }
   handleChange = (id) => {
-    return {
-      todos: prevState.todos.map(todo => {
-        if (todo.id === id) {
-          return {
-            ...todo,
-            completed: !todo.completed,
+    this.setState(prevState => {
+      return {
+        todos: prevState.todos.map(todo => {
+          if (todo.id === id) {
+            return {
+              ...todo,
+              completed: !todo.completed,
+            }
           }
-        }
-        return todo
-      }),
-    }
+          return todo
+        }),
+      }
+    });
   };
   delTodo = id => {
     this.setState({
@@ -62,12 +64,16 @@ class TodoContainer extends React.Component {
   };
   render() {
     return (
-      <div>
-        <Header />
-        <InputTodo addTodoProps={this.addTodoItem} />
-        <TodosList todos={this.state.todos} 
-        handleChangeProps={this.handleChange} 
-        deleteTodoProps={this.delTodo} />
+      <div className="container">
+        <div className="inner">
+          <Header />
+          <InputTodo addTodoProps={this.addTodoItem} />
+          <TodosList
+            todos={this.state.todos}
+            handleChangeProps={this.handleChange}
+            deleteTodoProps={this.delTodo}
+          />
+        </div>
       </div>
     );
   }
